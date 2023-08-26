e6d5fce8059d1dbf3b4786e9fb73360e2020ae54
Lalit Kumar <lalit.modi@datenwissen.com>
2023-08-26 19:27:42 +0530

    product component create

---
+++ components/Product/ProductCon.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProductCon = () => {
+  return (
+    <div>ProductCon</div>
+  )
+}
+
+export default ProductCon
--- pages/product/[productname].js
+++ pages/product/[productname].js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useRouter } from 'next/router';
 import PageHeading from '@/components/Base/PageHeading';
 import img from "../../public/Images/productImg.jpeg"
+import ProductCon from '@/components/Product/ProductCon';
 const productname= () => {
     const router = useRouter();
     const { productname} = router.query;
@@ -21,10 +22,17 @@ else{
 
   return (
     <div>
-    <PageHeading heading={heading} img={img} bredcurm="Product"/>
-    <h1>{productname}</h1>
+      <PageHeading heading={heading} img={img} bredcurm="Product" />
+      <div className="grid grid-cols-4 px-[5%]">
+        <div className=" col-span-3">
+          <ProductCon />
+        </div>
+        <div className="">
+        
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default productname
