09b807a124d3accc2cb676e522eb78570c09aa81
Lalit Kumar <lalit.modi@datenwissen.com>
2023-08-30 16:01:17 +0530

    facebook link added

--- components/Base/TopInfo.js
+++ components/Base/TopInfo.js
@@ -19,25 +19,58 @@ const TopInfo = () => {
     const url = `https://wa.me/${formattedPhoneNumber}`;
     window.open(url, '_blank');
   };
-  const handleInsta=()=>{
-    const url=''
-    // window.open(url, '_blank');
+  const handleInsta=(url)=>{
+    window.open(url, '_blank');
   }
-  return (
-    <div className='h-[50px] max-[767px]:h-[50px] w-full bg-footerBg text-white flex items-center justify-between px-[5%] text-[1.3rem]'>
-   <div className='flex md:items-center justify-between md:w-[60%] max-[767px]:flex-col'>
-   <span className='flex items-center gap-3 font-poppins min-[320px]:text-[0.9rem] xl:text-[1.2rem]  lg:text-[1rem] cursor-pointer ' onClick={handlePhoneClick}><FaPhone  className='text-mainRed' /> +91:{websiteData.contactInfo.mobileNo[0]}
-   </span>
-   <span className='flex items-center gap-3 font-poppins min-[320px]:text-[0.9rem] xl:text-[1.2rem]  lg:text-[1rem] cursor-pointer ' onClick={handleEmailClick}><FaEnvelope className='text-mainRed' /> {websiteData.contactInfo.email[0]} </span>
-   </div>
-    <div className='flex items-center gap-2 max-[767px]:text-[1.2rem] lg:text-[1.2rem]  xl:text-[1.2rem]'>
-    <span className='p-2 rounded-full text-mainRed bg-white hover:bg-mainRed hover:text-white cursor-pointer' onClick={()=>handleWhatsAppClick()} ><FaWhatsapp /></span>
-    <span className='p-2 rounded-full text-mainRed bg-white hover:bg-mainRed hover:text-white cursor-pointer' onClick={handleInsta} ><FaFacebook/></span>
-    <span className='p-2 rounded-full text-mainRed bg-white hover:bg-mainRed hover:text-white cursor-pointer' onClick={handleInsta}><FaInstagram/></span>
 
+  return (
+    <div className="h-[50px] max-[767px]:h-[50px] w-full bg-footerBg text-white flex items-center justify-between px-[5%] text-[1.3rem]">
+      <div className="flex md:items-center justify-between md:w-[60%] max-[767px]:flex-col">
+        <span
+          className="flex items-center gap-3 font-poppins min-[320px]:text-[0.9rem] xl:text-[1.2rem]  lg:text-[1rem] cursor-pointer "
+          onClick={handlePhoneClick}
+        >
+          <FaPhone className="text-mainRed" /> +91:
+          {websiteData.contactInfo.mobileNo[0]}
+        </span>
+        <span
+          className="flex items-center gap-3 font-poppins min-[320px]:text-[0.9rem] xl:text-[1.2rem]  lg:text-[1rem] cursor-pointer "
+          onClick={handleEmailClick}
+        >
+          <FaEnvelope className="text-mainRed" />{" "}
+          {websiteData.contactInfo.email[0]}{" "}
+        </span>
+      </div>
+      <div className="flex items-center gap-2 max-[767px]:text-[1.2rem] lg:text-[1.2rem]  xl:text-[1.2rem]">
+        <span
+          className="p-2 rounded-full text-mainRed bg-white hover:bg-mainRed hover:text-white cursor-pointer"
+          onClick={() => handleWhatsAppClick()}
+        >
+          <FaWhatsapp />
+        </span>
+        <span
+          className="p-2 rounded-full text-mainRed bg-white hover:bg-mainRed hover:text-white cursor-pointer"
+          onClick={() =>
+            handleInsta(
+              "https://www.facebook.com/profile.php?id=100064562211511&mibextid=ZbWKwL"
+            )
+          }
+        >
+          <FaFacebook />
+        </span>
+        <span
+          className="p-2 rounded-full text-mainRed bg-white hover:bg-mainRed hover:text-white cursor-pointer"
+          onClick={() =>
+            handleInsta(
+              "https://www.facebook.com/profile.php?id=100064562211511&mibextid=ZbWKwL"
+            )
+          }
+        >
+          <FaInstagram />
+        </span>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
 export default TopInfo
